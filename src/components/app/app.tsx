b1821864a26782f78bd83de85a689c5760baf9d3
alekseishkel <lexashkel@gmail.com>
2021-02-22 12:55:46 +0300

    fix: glue footer to the bottom of the screen

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,9 +3,24 @@ import React from "react";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+main {
+  flex: 1 0 auto;
+}
+`;
+
 const App: React.FC = () => {
   return (
     <React.Fragment>
+      <GlobalStyle />
       <Header />
       <Footer />
     </React.Fragment>
