f92dd4104f3e94ff1cda5123870da13d536f5e6e
alekseishkel <lexashkel@gmail.com>
2021-02-22 11:56:17 +0300

    fix: change footer & header color

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../header/header';
-import Footer from '../footer/footer';
+import Header from "../header/header";
+import Footer from "../footer/footer";
 
 const App: React.FC = () => {
   return (
@@ -9,7 +9,7 @@ const App: React.FC = () => {
       <Header />
       <Footer />
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const StyledHeader = styled.div`
+const StyledHeader = styled.nav`
   padding: 0 1%;
 `;
 
 const Header: React.FC = () => {
   return (
-      <nav>
-        <StyledHeader className="nav-wrapper light-blue">
-          <a href="#" className="brand-logo">Пятнашки</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Правила</a></li>
-          </ul>
-        </StyledHeader>
-      </nav>
-  )
-
-
-}
+    <header>
+      <StyledHeader className="nav-wrapper blue darken-3">
+        <a href="#" className="brand-logo">
+          Пятнашки
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Правила</a>
+          </li>
+        </ul>
+      </StyledHeader>
+    </header>
+  );
+};
 
 export default Header;
