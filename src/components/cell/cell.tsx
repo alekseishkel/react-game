3738d34140888971381eb75664aebcc3eafc2e7d
alekseishkel <lexashkel@gmail.com>
2021-02-26 12:22:47 +0300

    feat: add nearest cells replace with borders

--- src/components/cell/cell.tsx
+++ src/components/cell/cell.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 interface CellProps {
   cellIndex: number;
@@ -16,17 +16,35 @@ const StyledCell = styled.div`
   align-items: center;
   width: 100px;
   height: 100px;
-  background-color: #eceff1;
+  background-color: ${(props) => (props.allowed ? "#cfd8dc" : "#eceff1")};
   color: #000000;
   font-size: 35px;
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.7);
+  cursor: pointer;
 `;
+const Cell: React.FC<CellProps> = ({
+  cellIndex,
+  fieldSize,
+  number,
+  nullCellIndex,
+  onCellClick,
+}) => {
+  if (
+    ((nullCellIndex + 1) % fieldSize === 0 && cellIndex % fieldSize === 0) ||
+    (nullCellIndex % fieldSize === 0 && (cellIndex + 1) % fieldSize === 0)
+  ) {
+    return <StyledCell>{number}</StyledCell>;
+  }
 
-const Cell: React.FC<CellProps> = ({ cellIndex, fieldSize, number, nullCellIndex, onCellClick }) => {
-  if (nullCellIndex === cellIndex + 1 ||
+  if (
+    nullCellIndex === cellIndex + 1 ||
     nullCellIndex === cellIndex - 1 ||
     nullCellIndex === cellIndex + fieldSize ||
-    nullCellIndex === cellIndex - fieldSize) {
-    return <StyledCell onClick={() => onCellClick(number)}>{number}</StyledCell>;
+    nullCellIndex === cellIndex - fieldSize
+  ) {
+    return (
+      <StyledCell onClick={() => onCellClick(number)}>{number}</StyledCell>
+    );
   }
 
   return <StyledCell>{number}</StyledCell>;
